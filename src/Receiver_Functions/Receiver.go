4181f7e38d84458b53b02a04252909e430191905
Sumit Kala <sumitk31@gmail.com>
2020-08-01 12:42:43 +0530

    updating receiver example

---
+++ src/Receiver_Functions/Receiver.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Custom type declaration numbers is a slice of integers
+type numbers []int
+
+//Define a receiver function
+// The below function receives a slice of numbers and
+// returns another slice with all the numbers doubled.
+func (mynums numbers) myprint() numbers {
+	double_nums := numbers{}
+	for i, num := range mynums {
+		fmt.Println(i, num)
+		double_nums = append(double_nums, num*2)
+	}
+	return double_nums
+}
+
+func main() {
+	fmt.Println("Running program")
+	nums := numbers{1, 2, 3, 4, 5}
+	double_numbs := nums.myprint()
+	fmt.Println(" Printing the Double Slice")
+	//for i, num := range double_numbs {// This will give a compilation error since i is not used to avoid the error
+	// we can use _
+	for _, num := range double_numbs { // _ is used to ignore the index returned from range
+		fmt.Println(num)
+	}
+
+}
